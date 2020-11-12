7566cae07c9662eb296b572891463ee87b547bfe
Jimi <jimijenneskens@gmail.com>
2020-11-12 13:09:34 -0600

    corealmostfinished

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -30,6 +30,59 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
     healthData.forEach(function(data) {
       data.age = +data.age;
       data.smokes = +data.smokes;
-      console.log(data);
     });
-})
+  
+    // create scales
+    var xScale = d3.scaleLinear()
+      .domain(d3.extent(healthData, d => d.age))
+      .range([0, width])
+      .nice(); 
+  
+    var yScale = d3.scaleLinear()
+      .domain([6,d3.max(healthData, d => d.smokes)])
+      .range([height, 0])
+      .nice();
+    
+    // create Axes
+    var xAxis = d3.axisBottom(xScale);
+    var yAxis = d3.axisLeft(yScale);
+  
+  
+  // append axes
+    chartGroup.append("g").attr("transform", `translate(0, ${height})`).call(xAxis);
+    chartGroup.append("g").call(yAxis);
+  
+  // generate scatter plot
+  chartGroup.selectAll("circle")
+  .data(healthData)
+  .enter()
+  .append("circle")
+  .attr("cx", d=>xScale(d.age))
+  .attr("cy", d=>yScale(d.smokes))
+  .attr("r", 15)
+  .classed("stateCircle", true);
+  
+  // add text to each datapoint
+  chartGroup.append("g")
+    .selectAll('text')
+    .data(healthData)
+    .enter()
+    .append("text")
+    .text(d=>d.abbr)
+    .attr("x",d=>xScale(d.age))
+    .attr("y",d=>yScale(d.smokes))
+    .classed(".stateText", true);
+    
+    // add axes titles
+    chartGroup.append("text")
+          .attr("transform", `translate(${width / 2}, ${height + margin.top + 13})`)
+          .text("Median Age");
+  
+          chartGroup.append("text")
+          .attr("y", 0 - ((margin.left / 2) + 2))
+          .attr("x", 0 - (height / 2))
+          .attr("transform", "rotate(-90)")
+          .text("Smokers (%)");
+  }).catch(function(err) {
+    console.log(err);
+  });
